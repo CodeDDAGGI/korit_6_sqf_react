b16fe68e4b32c5f3d6666ae8ef20f5c9b8218050
CodeDDAGGI <qudrb1132@naver.com>
2024-06-19 17:53:41 +0900

    0619

--- src/ch04/Button.js
+++ src/ch04/Button.js
@@ -1,15 +1,33 @@
-function Button({ text , setNumber}) {
+// function Button({ text , setNumber}) {
   
-  const handleClick = () => {
-    if(text === "증가"){
-        setNumber(number => number + 1);
-    }
-    if(text === "감소"){
-        setNumber(number => number - 1);
-    }
-  } 
+//   const handleClick = () => {
+//     if(text === "증가"){
+//         setNumber(number => number + 1);
+//     }
+//     if(text === "감소"){
+//         setNumber(number => number - 1);
+//     }
+//   } 
    
-  return <button onClick={handleClick}>{text}</button>
-}
+//   return <button onClick={handleClick}>{text}</button>
+// }
 
-export default Button;
+// export default Button;
+
+function Button({ text ,number, setNumber}) {
+  
+    const handleClick = () => {
+      if(text === "증가"){
+          setNumber(200);
+      }
+      if(text === "감소"){
+          setNumber(200);
+      }
+    } 
+     
+    return <button onClick={handleClick}>{text}</button>
+  }
+  
+  export default Button;
+  
+  
