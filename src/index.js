ec7fa434f1cab5333a30ed059c40b6d8016dfd82
CodeDDAGGI <qudrb1132@naver.com>
2024-06-20 23:17:57 +0900

    0620저녁

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './ch04/App'; // 기본틀 (다른 컴포넌트사용가능)
+import App from './ch10/App'; // 기본틀 (다른 컴포넌트사용가능)
 
 //? public > index.html > div id= root (최상위)
 const root = ReactDOM.createRoot(document.getElementById('root'));
